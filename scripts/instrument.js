a1ea8cbaec14db39abbfbed1520634257f89b184
Mikko Pohja <mikko.pohja@futurice.com>
2015-02-21 00:01:18 +0200

    Further development of a proper instrument

--- scripts/instrument.js
+++ scripts/instrument.js
@@ -5,11 +5,14 @@ define([
     ac,
 	envelope
 ) {
+    // TODO: create an ultimate synthesizer object with chainable api
+    // like inst.oscType("sawtooth").envelope(0.1,0.4,0.8,2).filter("lowpass", 500)
+
     var sine = function() {
         var sine = ac.createOscillator();
         var amp = ac.createGain();
         sine.connect(amp);
-        amp.connect(ac.destination);
+        
         return { 
             node: sine, 
             amp: amp 
@@ -24,9 +27,17 @@ define([
     // AudioNode node: giving the output node not connected to anywhere
     // function(note, time, length) play: function for scheduling a note
     return { 
-        createSineADSR: function(attack, decay, sustain, release) {
+        new: function() {
+        	console.log("ASD");
+        },
+        createOscADSR: function createOscADSR(attack, decay, sustain, release, oscType) {
             var s = sine();
+            // TODO: custom oscillator
+            if (["sine", "square", "sawtooth", "triangle"].indexOf(oscType) !== -1) {
+                s.node.type = oscType;
+            }
             s.amp.gain.setValueAtTime(0, ac.currentTime);
+            s.amp.name = "OscADSR amp";
             var e = new Envelope(ac, attack, decay, sustain, release);
             e.connect(s.amp.gain);
             s.node.start();
@@ -41,6 +52,48 @@ define([
             return {
                 play: playFn,
                 node: s.amp
+            };
+        },
+        createDrum: function(decay, startFreq, endFreq, duration) {
+			var decay = decay ? decay: 10;
+			var start = start ? start: 200;
+			var end = end ? end: 40;
+			var duration = duration ? duration: 300;
+			var sine = ac.createOscillator();
+			sine.name = "Drum sine";
+            var amp = ac.createGain();
+			amp.name = "Drum amp";
+            sine.type = "sine";
+			sine.connect(amp);
+			sine.noteOn(ac.currentTime);
+			amp.gain.setValueAtTime(0, ac.currentTime);
+            
+            var playFn =  function(note, time, length) { 
+				sine.frequency.setValueAtTime(start, time);
+				sine.frequency.exponentialRampToValueAtTime(end, time + (1/decay));
+				amp.gain.setValueAtTime(0, time);
+				amp.gain.linearRampToValueAtTime(1, time + 0.001);
+				amp.gain.setValueAtTime(1, time + 0.3 * duration);
+				amp.gain.linearRampToValueAtTime(0, time + duration/1000);
+            }
+
+			return {
+                play: playFn,
+                node: amp
+            };
+        },
+        createLPOsc: function(limit) {
+            var osc = this.createOscADSR(0.2, 0.4, 0.2, 4, "sawtooth");
+			var filter = ac.createBiquadFilter();
+			filter.type = "lowpass";
+			filter.frequency.value = limit;
+			filter.gain.value = 25;
+
+			osc.node.connect(filter);
+
+            return {
+                play: osc.play,
+                node: filter
             };
         }
     };
